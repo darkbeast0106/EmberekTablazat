fa4fc3a3c9175c8cc3965afcc1dcd9a6022595c7
Budaházi Bence <budahazi.bence@petrik.hu>
2022-11-08 14:55:11 +0100

    Hozzáadás, törlés

--- src/main/java/hu/petrik/emberektablazat/EmberekController.java
+++ src/main/java/hu/petrik/emberektablazat/EmberekController.java
@@ -1,9 +1,8 @@
 package hu.petrik.emberektablazat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EmberekController {
@@ -13,6 +12,10 @@ public class EmberekController {
     private TableColumn<Ember, String> nevOszlop;
     @FXML
     private TableColumn<Ember, Integer> korOszlop;
+    @FXML
+    private TextField nevMezo;
+    @FXML
+    private Spinner<Integer> korMezo;
 
     @FXML
     private void initialize() {
@@ -24,5 +27,26 @@ public class EmberekController {
         emberek.getItems().add(e1);
         emberek.getItems().add(e2);
         emberek.getItems().add(e3);
+    }
+
+    @FXML
+    private void hozzaadClick() {
+        String nev = nevMezo.getText().trim();
+        int kor = korMezo.getValue();
+        Ember e = new Ember(nev, kor);
+        emberek.getItems().add(e);
+    }
+
+    @FXML
+    private void torlesClick() {
+        int index = emberek.getSelectionModel().getSelectedIndex();
+        if (index == -1){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Törléshez előbb válasszon ki elemet");
+            alert.show();
+            return;
+        }
+        Ember torlendoEmber = emberek.getSelectionModel().getSelectedItem();
+        emberek.getItems().remove(torlendoEmber);
     }
 }
